b00912469cc1c54c9c6494685027a335147af011
shafiquddin <shafiquddin2k@gmail.com>
2024-02-10 19:23:55 +0530

    memo and composition on components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfiguredCounter from "./components/Counter/configuredCounter.jsx";
+
+function App() {
+  log("<App /> rendered");
+
+  const [chosenCount, setChosenCount] = useState(0);
+
+  const handleSetCount = (count) => {
+    setChosenCount(count);
+  };
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfiguredCounter onSet={handleSetCount} />
+        <Counter initialCount={chosenCount} />
+      </main>
+    </>
+  );
+}
+
+export default App;
